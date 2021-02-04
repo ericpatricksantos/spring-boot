54ce25d5fda41ed463092a218a692b4d999c030c
ericpatricksantos <ericpatrick.santos@ufu.br>
2021-02-04 04:29:50 -0300

    Serviços e Registros de Componentes

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course.entities.User;
 
+
+@Repository // Nesse caso, essa anotação é opcional, porque esta herdando da classe JPARepository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	
