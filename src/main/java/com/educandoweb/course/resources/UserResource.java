31b584c571a11fac7d0706f477259a5a5647ecd6
ericpatricksantos <ericpatrick.santos@ufu.br>
2021-02-06 13:06:50 -0300

    insert user

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,19 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -31,5 +37,13 @@ public class UserResource {
 	public ResponseEntity<User>  findById(@PathVariable Long id) {
 		User obj =  service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	//Endpoint 
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){ // @RequestBody -> usada para dizer que o objeto(User obj) vai chegar no modo json na hora de fazer requisição e esse Json será desserializado para um objeto User do java 
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
